aad5cb3495c18a42de19b59acde3194683c57b11
mannycerna <manuel.cerna@codeimmersives.com>
2022-10-24 18:00:07 -0500

    adding files

---
+++ main.js
@@ -0,0 +1,27 @@
+//Change the font color of the paragraph to light blue.
+
+let para = document.querySelector('p');
+para.style.color='lightblue';
+
+
+let head = document.querySelector('h1');
+head.style.fontSize = '48px';
+
+para.innerText = "Lorem ipsum dolor amet viral meh selfies drinking vinegar, intelligentsia poke flannel twee paleo enamel pin cray. Banjo celiac crucifix, kickstarter la croix air plant jianbing hashtag vinyl hell of man bun selvage schlitz banh mi. Tacos hella raclette quinoa blog, williamsburg adaptogen tbh. Hexagon af stumptown lumbersexual synth gentrify quinoa enamel pin celiac master cleanse. Truffaut typewriter shoreditch, semiotics iceland mixtape taxidermy umami distillery austin hashtag. Food truck synth wayfarers, street art banh mi actually authentic. Bitters tousled tattooed vegan neutra pug hell of fixie chia unicorn letterpress.";
+
+let item13 = document.querySelector("#item-13");
+item13.style.opacity = '0.5';
+let item3 = document.querySelector("#item-3");
+item3.innerText = "I say, Hi!";
+
+let myImage = document.querySelector("img");
+myImage.src = "https://cdn.pixabay.com/photo/2018/01/12/10/19/fantasy-3077928__480.jpg";
+myImage.style.height = '300px';
+
+let myImage2 = document.querySelector("#image-2");
+myImage2.style.height = '300px';
+
+let item16 = document.querySelector("#item-16");
+item16.classList.add("item");
+item16.innerText = "Won't get fooled again."
+
